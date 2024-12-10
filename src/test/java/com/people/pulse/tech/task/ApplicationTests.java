9271ba399faf210a1f9165acce3c3accca3c1218
JadeRowan <rougesoul.m@gmail.com>
2024-12-10 17:09:31 +0200

    Initial commit

---
+++ src/test/java/com/people/pulse/tech/task/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.pulse.tech.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
